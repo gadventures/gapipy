9d1bbbc5813b47810824339872aa3b9be5086248
Bartek Ciszkowski <bart.ciszk@gmail.com>
2014-09-23 11:52:47 -0400

    Add new Language fields, update version.

--- gapipy/resources/language.py
+++ gapipy/resources/language.py
@@ -7,7 +7,7 @@ class Language(Resource):
 
     _resource_name = 'languages'
 
-    _as_is_fields = ['id', 'href', 'name']
+    _as_is_fields = ['id', 'href', 'name', 'iso_639_3', 'iso_639_1']
 
     def __repr__(self):
         return '<{}: {}>'.format(self.__class__.__name__, self.name)
