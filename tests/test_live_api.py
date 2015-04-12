024dd52f71b5078a352fa8bc49fb426facab570c
Vida <vsadri@gadventures.com>
2015-04-11 21:39:18 -0400

    Began working on porting to Python3
    Got inspired by Pycon and wanted to practice some Pycon skills. Thanks to Barteck.

--- tests/test_live_api.py
+++ tests/test_live_api.py
@@ -67,7 +67,7 @@ class TourDossierTestCase(TestCase):
 
     def test_get_image_url(self):
         url = self.dossier.get_map_url()
-        self.assertIsInstance(url, basestring)
+        self.assertIsInstance(url, items)
 
     def test_get_countries(self):
         countries = self.dossier.get_visited_countries()
@@ -75,7 +75,7 @@ class TourDossierTestCase(TestCase):
 
     def test_get_trip_detail(self):
         detail = self.dossier.get_trip_detail('Max Pax')
-        self.assertIsInstance(detail, basestring)
+        self.assertIsInstance(detail, items)
 
 
 @attr('integration')
