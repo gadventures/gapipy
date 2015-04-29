59f560e331dbeb21b353cad8386933562cfb048b
Bartek Ciszkowski <bart.ciszk@gmail.com>
2015-04-29 17:05:28 -0400

    Added description to TourCategory model

--- gapipy/__init__.py
+++ gapipy/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.41'
+__version__ = '0.1.42'
 __title__ = 'gapipy'
 
 
--- gapipy/resources/tour/tour_category.py
+++ gapipy/resources/tour/tour_category.py
@@ -7,5 +7,5 @@ class TourCategory(Resource):
 
     _resource_name = 'tour_categories'
 
-    _as_is_fields = ['id', 'href', 'name']
+    _as_is_fields = ['id', 'href', 'name', 'description']
     _resource_fields = [('category_type', 'TourCategory')]
