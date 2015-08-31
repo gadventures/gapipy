6b7abaf2d5793dd8b4ab3804fbda0a0a3035f1ed
Rafik Draoui <rafikd@gadventures.com>
2015-08-31 12:30:09 -0400

    Add `Departure.components`

--- gapipy/resources/tour/departure.py
+++ gapipy/resources/tour/departure.py
@@ -4,12 +4,14 @@ from ...models import Address, AddOn, DepartureRoom, PP2aPrice
 
 from ..base import Product
 from .tour_dossier import TourDossier
+from .departure_component import DepartureComponent
 
 
 class Departure(Product):
 
     _resource_name = 'departures'
     _is_listable = True
+    _is_parent_resource = True
 
     _as_is_fields = [
         'id', 'href', 'availability', 'flags', 'nearest_start_airport',
@@ -19,6 +21,9 @@ class Departure(Product):
     _date_time_fields_utc = ['date_created', 'date_last_modified']
     _date_time_fields_local = ['latest_arrival_time', 'earliest_departure_time']
     _resource_fields = [('tour', 'Tour'), ('tour_dossier', TourDossier)]
+    _resource_collection_fields = [
+        ('components', DepartureComponent),
+    ]
     _model_fields = [('start_address', Address), ('finish_address', Address)]
     _model_collection_fields = [
         ('addons', AddOn),
