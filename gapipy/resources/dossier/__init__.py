e365c6015315f904a84ee0fa96df0eacd735e8b4
annakovale <akovalevskaya@gadventures.com>
2016-03-10 15:36:46 -0500

    Implemented the Recourse model for DossierFeatures

--- gapipy/resources/dossier/__init__.py
+++ gapipy/resources/dossier/__init__.py
@@ -4,3 +4,4 @@ from .accommodation_dossier import AccommodationDossier
 from .activity_dossier import ActivityDossier
 from .place_dossier import PlaceDossier
 from .transport_dossier import TransportDossier
+from .dossier_features import DossierFeature
