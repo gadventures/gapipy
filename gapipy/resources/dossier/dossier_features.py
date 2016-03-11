07a1bf9fcb12dbf743a28b7a12576a7bc3ae7977
Bartek Ciszkowski <bart.ciszk@gmail.com>
2016-03-11 09:36:12 -0500

    Adjusted field name on DossierFeature to children
    Prior, child, was incorrect.

--- gapipy/resources/dossier/dossier_features.py
+++ gapipy/resources/dossier/dossier_features.py
@@ -1,12 +1,15 @@
 from ..base import Resource
 from ...models import DossierFeatureParent, DossierFeatureChild
 
+
 class DossierFeature(Resource):
     _resource_name = 'dossier_features'
     _as_is_fields = ['id', 'href', 'label', 'code', 'description']
 
-    _model_fields = [('parent', DossierFeatureParent),]
+    _model_fields = [
+        ('parent', DossierFeatureParent),
+    ]
 
     _model_collection_fields = [
-        ('child', DossierFeatureChild),
+        ('children', DossierFeatureChild),
     ]
