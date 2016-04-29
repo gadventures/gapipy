5c489f9dac99e33cc2a73fc713e116a5a8cf5368
Bartek Ciszkowski <bart.ciszk@gmail.com>
2016-04-29 08:13:56 -0400

    Remove emails from AgencyChain
    Invalid field addition.

--- gapipy/resources/booking/agency_chain.py
+++ gapipy/resources/booking/agency_chain.py
@@ -4,5 +4,5 @@ from ..base import Resource
 class AgencyChain(Resource):
 
     _resource_name = 'agency_chains'
-    _as_is_fields = ['id', 'href', 'name', 'flags', 'communication_preferences', 'payment_options', 'emails', 'agencies']
+    _as_is_fields = ['id', 'href', 'name', 'flags', 'communication_preferences', 'payment_options', 'agencies']
     _date_time_fields_local = ['date_created']
