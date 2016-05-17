13a2ea421b761b9009fb7e1328e54cf0ae5cc54f
Bartek Ciszkowski <bart.ciszk@gmail.com>
2016-05-17 15:38:18 -0400

    Add new Agency resource fields

--- gapipy/resources/booking/agency.py
+++ gapipy/resources/booking/agency.py
@@ -2,10 +2,15 @@ from __future__ import unicode_literals
 
 from ...models import Address
 from ...models import AgencyDocument
+from ...models.base import BaseModel
 from .agency_chain import AgencyChain
 
 from ..base import Resource
 from ..tour import Promotion
+
+
+class AgencyEmail(BaseModel):
+    _as_is_fields = ['type', 'address']
 
 
 class Agency(Resource):
@@ -13,11 +18,14 @@ class Agency(Resource):
     _is_listable = False
     _is_parent_resource = True
 
-    _as_is_fields = ['id', 'href', 'name', 'booking_currencies', 'latitude', 'longitude']
+    _as_is_fields = ['id', 'href', 'name', 'booking_currencies', 'latitude', 'longitude', 'transactional_email']
     _date_time_fields_local = ['date_created']
     _model_fields = [('address', Address)]
     _resource_fields = [('agency_chain', AgencyChain)]
-    _model_collection_fields = [('documents', AgencyDocument)]
+    _model_collection_fields = [
+        ('documents', AgencyDocument),
+        ('emails', AgencyEmail),
+    ]
     _resource_collection_fields = [
         ('bookings', 'Booking'),
         ('agents', 'Agent'),
