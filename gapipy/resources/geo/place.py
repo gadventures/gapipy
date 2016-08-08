6195c2c510cf8997e596742d082e195c93c75eb5
Sia <siavash.malekpour@gmail.com>
2016-08-08 10:52:58 -0400

    Add `places_of_interest` to Place Resource

--- gapipy/resources/geo/place.py
+++ gapipy/resources/geo/place.py
@@ -16,7 +16,7 @@ class Place(Resource):
     _as_is_fields = [
         'id', 'href', 'name', 'ascii_name', 'population', 'elevation',
         'latitude', 'longitude', 'bounding_box', 'alternate_names',
-        'admin_divisions',
+        'admin_divisions', 'places_of_interest',
     ]
     _date_time_fields_utc = ['date_created', 'date_last_modified']
     _resource_fields = [
