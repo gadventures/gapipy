beeae2daf35da275d5f9e1ad01516c917319bf00
Bartek Ciszkowski <bart.ciszk@gmail.com>
2017-01-20 09:01:26 -0500

    Add Place reference to State model

--- gapipy/resources/geo/state.py
+++ gapipy/resources/geo/state.py
@@ -9,7 +9,10 @@ class State(Resource):
     _resource_name = 'states'
 
     _as_is_fields = ['id', 'href', 'name']
-    _resource_fields = [('country', 'Country')]
+    _resource_fields = [
+        ('country', 'Country'),
+        ('place', 'Place'),
+    ]
 
     @enforce_string_type
     def __repr__(self):
