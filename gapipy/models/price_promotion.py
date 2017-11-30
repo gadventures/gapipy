63ffb8da5bf6bbdbdc3b87bad3d2021a0afe1758
Anna Kovalevskaya <anna.kovalevskaya@mail.utoronto.ca>
2017-11-30 15:42:36 -0500

    Explicitely copy fields

--- gapipy/models/price_promotion.py
+++ gapipy/models/price_promotion.py
@@ -14,16 +14,28 @@ class PricePromotion(Resource):
 
         klass = get_resource_class_from_resource_name('promotions')
 
+        promotion_field_types = [
+            '_as_is_fields',
+            '_date_time_fields_utc',
+            '_date_fields',
+            '_price_fields',
+            '_model_collection_fields',
+            '_resource_collection_fields',
+        ]
+
         # Python 2 and 3
         # inefficient on Python 2 to list items()
-        for k, v in list(klass.__dict__.items()):
-            if 'fields' in k:
-                setattr(self, k, v)
-            if '_is_parent_resource' in k:
-                setattr(self, k, v)
+        for field_type in promotion_field_types:
+            value = getattr(klass, field_type, None)
+            if value:
+                setattr(self, field_type, value)
+
+        if getattr(klass, '_is_parent_resource', None):
+            setattr(self, klass._is_parent_resource)
 
         super(PricePromotion, self).__init__(data, **kwargs)
 
         # Add the "fake" amount field.
-        self._price_fields.append('amount')
-        setattr(self, 'amount', data['amount'])
+        if 'amount' not in self._price_fields:
+            self._price_fields.append('amount')
+            setattr(self, 'amount', data['amount'])
