d3de17571e56d567fc3a66cc98f4b6e309516f23
Jon Prindiville <jon.prindiville@gmail.com>
2018-11-07 10:32:50 -0500

    Add validity-checking methods... (#99)
    ... to Itinerary and ValidDuringRanges. Also, add some tests.

--- gapipy/resources/tour/itinerary.py
+++ gapipy/resources/tour/itinerary.py
@@ -2,6 +2,8 @@
 # Python 2 and 3
 from __future__ import unicode_literals
 
+import datetime
+
 from .image import Image, MAP_TYPE
 from ..base import Resource
 from ...models.base import BaseModel
@@ -90,6 +92,72 @@ class ItineraryDay(BaseModel):
 
 class ValidDuringRange(BaseModel):
     _date_fields = ['start_date', 'end_date']
+
+    def is_expired(self):
+        return not self.is_valid_during_range(datetime.date.today(), None)
+
+    def is_valid_today(self):
+        return self.is_valid_on_date(datetime.date.today())
+
+    def is_valid_during_range(self, start_date, end_date):
+        if start_date and not end_date:
+            return self.is_valid_on_or_after_date(start_date)
+
+        if not start_date and end_date:
+            return self.is_valid_on_or_before_date(end_date)
+
+        if not start_date and not end_date:
+            return self.is_valid_sometime()
+
+        if start_date == end_date:
+            return self.is_valid_on_date(start_date)
+
+        # start_date and end_date are both not None and not equal
+        return (
+            start_date <= end_date
+        ) and (
+            self.is_valid_sometime()
+        ) and all([
+            self.start_date is None or self.start_date <= end_date,
+            self.end_date is None or self.end_date >= start_date,
+        ])
+
+    def is_valid_on_or_after_date(self, date):
+        return (
+            self.end_date is None
+        ) or (
+            self.start_date is None and
+            self.end_date >= date
+        ) or (
+            self.start_date is not None and
+            self.start_date <= self.end_date and
+            self.end_date >= date
+        )
+
+    def is_valid_on_or_before_date(self, date):
+        return (
+            self.start_date is None
+        ) or (
+            self.end_date is None and
+            self.start_date <= date
+        ) or (
+            self.end_date is not None and
+            self.start_date <= self.end_date and
+            self.start_date <= date
+        )
+
+    def is_valid_on_date(self, date):
+        return all([
+            self.start_date is None or self.start_date <= date,
+            self.end_date is None or self.end_date >= date,
+        ])
+
+    def is_valid_sometime(self):
+        return (
+            self.start_date is None or
+            self.end_date is None or
+            self.start_date <= self.end_date
+        )
 
     @enforce_string_type
     def __repr__(self):
@@ -192,3 +260,45 @@ class Itinerary(Resource):
         if not image or not getattr(image, 'file', None):
             return None
         return image.file.url
+
+    def is_expired(self):
+        return all([
+            vrange.is_expired()
+            for vrange in self.valid_during_ranges
+        ])
+
+    def is_valid_today(self):
+        return any([
+            vrange.is_valid_today()
+            for vrange in self.valid_during_ranges
+        ])
+
+    def is_valid_during_range(self, start_date, end_date):
+        return any([
+            vrange.is_valid_during_range(start_date, end_date)
+            for vrange in self.valid_during_ranges
+        ])
+
+    def is_valid_on_or_after_date(self, date):
+        return any([
+            vrange.is_valid_on_or_after_date(date)
+            for vrange in self.valid_during_ranges
+        ])
+
+    def is_valid_on_or_before_date(self, date):
+        return any([
+            vrange.is_valid_on_or_before_date(date)
+            for vrange in self.valid_during_ranges
+        ])
+
+    def is_valid_on_date(self, date):
+        return any([
+            vrange.is_valid_on_date(date)
+            for vrange in self.valid_during_ranges
+        ])
+
+    def is_valid_sometime(self):
+        return any([
+            vrange.is_valid_sometime()
+            for vrange in self.valid_during_ranges
+        ])
